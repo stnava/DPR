81ba9d22100f291c0b80fb98072c192012752b62
stnava <stnava@gmail.com>
2022-09-15 10:20:42 -0400

    WIP: ... training script ...

--- src/dpr_train.py
+++ src/dpr_train.py
@@ -525,12 +525,8 @@ if os.path.isfile(ofn):
     mdl = tf.keras.models.load_model( ofn, compile=False )
 
 # set an optimizer - just standard Adam - may be sensitive to learning_rate
-
-# In[86]:
-
-
 ct = 0
-opt = tf.keras.optimizers.Adam(learning_rate=1e-4)
+opt = tf.keras.optimizers.Adam(learning_rate=1e-5)
 
 
 # model compilation
