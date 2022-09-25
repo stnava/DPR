5d6b84d0434a35c0f792acb28ee553d7556cf5dd
stnava <stnava@gmail.com>
2022-09-25 05:19:39 -0400

    STYLE: reorg code a bit

--- src/dsr_3d_inference.py
+++ src/dsr_3d_inference.py
@@ -49,9 +49,13 @@ def get_random_patch_pair( img, img2, patchWidth ):
             return myimg, myimg2
     return myimg, myimg2
 
+ofn = 'models/dsr3d_2up_64_256_6_1_2_v0.0.h5'
+mdl1 = tf.keras.models.load_model( ofn, compile=False )
+ofn = '/Users/stnava/.antspymm/brainSR.h5'
+mdl2 = tf.keras.models.load_model( ofn, compile=False )
+ofn = '/Users/stnava/data/examples_in_process/multi-modality-sr/models/brainSRHR.h5'
 ofn = 'models/dsr3d_2up_64_256_6_3_v0.0.h5'
-mdl = tf.keras.models.load_model( ofn, compile=False )
-
+mdl3 = tf.keras.models.load_model( ofn, compile=False )
 print("assemble images", flush=True )
 imgfns = glob.glob( "/raid/data_BA/brains/HCP/T*w/*nii.gz") + glob.glob( "/raid/data_BA/brains/HCP/T*w/*nii.gz")
 if len(imgfns) == 0:
@@ -59,7 +63,7 @@ if len(imgfns) == 0:
 random.shuffle(imgfns)
 print( imgfns[0] )
 img = ants.image_read( imgfns[0] )
-img1, img2 = get_random_patch_pair( img, img, patchWidth=96 )
+img1, img2 = get_random_patch_pair( img, img, patchWidth=64 )
 ants.plot(img1)
 spc = ants.get_spacing( img1 )
 newspc = []
@@ -70,23 +74,35 @@ img1lo = ants.resample_image( img1, newspc )
 img1up = ants.resample_image( img1lo, spc )
 
 print('begin sr : psnr orig = ' + str( antspynet.psnr(img1,img1up) ) )
-sr = antspynet.apply_super_resolution_model_to_image(
-    img1lo,
-    mdl,
-    target_range=[0,1], regression_order=None )
-# some metrics on the output
-gmsdSR = antspynet.gmsd(img1,sr)
-gmsdBi = antspynet.gmsd(img1,img1up)
-ssimSR = antspynet.ssim(img1,sr)
-ssimBi = antspynet.ssim(img1,img1up)
-psnrSR = antspynet.psnr(img1,sr)
-psnrBi = antspynet.psnr(img1,img1up)
-print("PSNR Test: " + str( psnrBi ) + " vs SR: " + str( psnrSR ), flush=True  )
-print("GMSD Test: " + str( gmsdBi ) + " vs SR: " + str( gmsdSR ), flush=True  )
-print("ssim Test: " + str( ssimBi ) + " vs SR: " + str( ssimSR ), flush=True  )
+blw=0.75
+for mdl in [mdl1,mdl2,mdl3]:
+    ranger = [0,1]
+    if mdl == mdl2:
+        ranger = [-127.5,127.5]
+    sr = antspynet.apply_super_resolution_model_to_image(
+        img1lo,
+        mdl,
+        target_range=ranger, regression_order=None )
+    sr = sr * blw + ants.iMath(sr,"Sharpen") * (1.0-blw)
+    # some metrics on the output
+    print("*****************************************")
+    gmsdSR = antspynet.gmsd(img1,sr)
+    gmsdBi = antspynet.gmsd(img1,img1up)
+    ssimSR = antspynet.ssim(img1,sr)
+    ssimBi = antspynet.ssim(img1,img1up)
+    psnrSR = antspynet.psnr(img1,sr)
+    psnrBi = antspynet.psnr(img1,img1up)
+    print("PSNR Test: " + str( psnrBi ) + " vs SR: " + str( psnrSR ), flush=True  )
+    print("GMSD Test: " + str( gmsdBi ) + " vs SR: " + str( gmsdSR ), flush=True  )
+    print("ssim Test: " + str( ssimBi ) + " vs SR: " + str( ssimSR ), flush=True  )
+
+
 ants.plot( img1 )
 ants.plot( img1up )
 ants.plot( sr )
+ants.image_write( img1, '/tmp/or.nii.gz' )
+ants.image_write( sr, '/tmp/srhi.nii.gz' )
+
 
 # now apply to "real" case
 srhi = antspynet.apply_super_resolution_model_to_image(
@@ -94,5 +110,4 @@ srhi = antspynet.apply_super_resolution_model_to_image(
     mdl,
     target_range=[0,1], regression_order=None )
 
-ants.image_write( img1, '/tmp/or.nii.gz' )
 ants.image_write( srhi, '/tmp/srhi.nii.gz' )
--- src/dsr_train_3d.py
+++ src/dsr_train_3d.py
@@ -16,8 +16,7 @@ import random
 # get_ipython().system('pip install git+https://github.com/ANTsX/ANTsPyNet')
 import ants
 
-
-# In[66]:
+import src.dbpn_arch
 
 
 import ants
@@ -130,201 +129,6 @@ offsetIntensity = 127.5
 # load in relevant objects from keras so we can build our network
 
 
-from keras.models import Model
-from keras.layers import (Input, Add, Subtract,
-                          PReLU, Concatenate,
-                          UpSampling2D, UpSampling3D,
-                          Conv2D, Conv2DTranspose,
-                          Conv3D, Conv3DTranspose)
-
-
-# define the DBPN network - this uses a model definition that is general to<br>
-# both 2D and 3D. recommended parameters for different upsampling rates can<br>
-# be found in the papers by Haris et al.  We make one significant change to<br>
-# the original architecture by allowing standard interpolation for upsampling<br>
-# instead of convolutional upsampling.  this is controlled by the interpolation<br>
-# option.
-
-# In[74]:
-
-
-def dbpn(input_image_size,
-                                                 number_of_outputs=1,
-                                                 number_of_base_filters=64,
-                                                 number_of_feature_filters=256,
-                                                 number_of_back_projection_stages=7,
-                                                 convolution_kernel_size=(12, 12),
-                                                 strides=(8, 8),
-                                                 last_convolution=(3, 3),
-                                                 number_of_loss_functions=1,
-                                                 interpolation = 'nearest'
-                                                ):
-    idim = len( input_image_size ) - 1
-    if idim == 2:
-        myconv = Conv2D
-        myconv_transpose = Conv2DTranspose
-        myupsampling = UpSampling2D
-        shax = ( 1, 2 )
-        firstConv = (3,3)
-        firstStrides=(1,1)
-        smashConv=(1,1)
-    if idim == 3:
-        myconv = Conv3D
-        myconv_transpose = Conv3DTranspose
-        myupsampling = UpSampling3D
-        shax = ( 1, 2, 3 )
-        firstConv = (3,3,3)
-        firstStrides=(1,1,1)
-        smashConv=(1,1,1)
-    def up_block_2d(L, number_of_filters=64, kernel_size=(12, 12), strides=(8, 8),
-                    include_dense_convolution_layer=True):
-        if include_dense_convolution_layer == True:
-            L = myconv(filters = number_of_filters,
-                       use_bias=True,
-                       kernel_size=smashConv,
-                       strides=firstStrides,
-                       padding='same')(L)
-            L = PReLU(alpha_initializer='zero',
-                      shared_axes=shax)(L)
-        # Scale up
-        if idim == 2:
-            H0 = myupsampling( size = strides, interpolation=interpolation )(L)
-        if idim == 3:
-            H0 = myupsampling( size = strides )(L)
-        H0 = myconv(filters=number_of_filters,
-                    kernel_size=firstConv,
-                    strides=firstStrides,
-                    use_bias=True,
-                    padding='same')(H0)
-        H0 = PReLU(alpha_initializer='zero',
-                   shared_axes=shax)(H0)
-        # Scale down
-        L0 = myconv(filters=number_of_filters,
-                    kernel_size=kernel_size,
-                    strides=strides,
-                    kernel_initializer='glorot_uniform',
-                    padding='same')(H0)
-        L0 = PReLU(alpha_initializer='zero',
-                   shared_axes=shax)(L0)
-        # Residual
-        E = Subtract()([L0, L])
-        # Scale residual up
-        if idim == 2:
-            H1 = myupsampling( size = strides, interpolation=interpolation  )(E)
-        if idim == 3:
-            H1 = myupsampling( size = strides )(E)
-        H1 = myconv(filters=number_of_filters,
-                    kernel_size=firstConv,
-                    strides=firstStrides,
-                    use_bias=True,
-                    padding='same')(H1)
-        H1 = PReLU(alpha_initializer='zero',
-                   shared_axes=shax)(H1)
-        # Output feature map
-        up_block = Add()([H0, H1])
-        return up_block
-    def down_block_2d(H, number_of_filters=64, kernel_size=(12, 12), strides=(8, 8),
-                    include_dense_convolution_layer=True):
-        if include_dense_convolution_layer == True:
-            H = myconv(filters = number_of_filters,
-                       use_bias=True,
-                       kernel_size=smashConv,
-                       strides=firstStrides,
-                       padding='same')(H)
-            H = PReLU(alpha_initializer='zero',
-                      shared_axes=shax)(H)
-        # Scale down
-        L0 = myconv(filters=number_of_filters,
-                    kernel_size=kernel_size,
-                    strides=strides,
-                    kernel_initializer='glorot_uniform',
-                    padding='same')(H)
-        L0 = PReLU(alpha_initializer='zero',
-                   shared_axes=shax)(L0)
-        # Scale up
-        if idim == 2:
-            H0 = myupsampling( size = strides, interpolation=interpolation )(L0)
-        if idim == 3:
-            H0 = myupsampling( size = strides )(L0)
-        H0 = myconv(filters=number_of_filters,
-                    kernel_size=firstConv,
-                    strides=firstStrides,
-                    use_bias=True,
-                    padding='same')(H0)
-        H0 = PReLU(alpha_initializer='zero',
-                   shared_axes=shax)(H0)
-        # Residual
-        E = Subtract()([H0, H])
-        # Scale residual down
-        L1 = myconv(filters=number_of_filters,
-                    kernel_size=kernel_size,
-                    strides=strides,
-                    kernel_initializer='glorot_uniform',
-                    padding='same')(E)
-        L1 = PReLU(alpha_initializer='zero',
-                   shared_axes=shax)(L1)
-        # Output feature map
-        down_block = Add()([L0, L1])
-        return down_block
-    inputs = Input(shape=input_image_size)
-    # Initial feature extraction
-    model = myconv(filters=number_of_feature_filters,
-                   kernel_size=firstConv,
-                   strides=firstStrides,
-                   padding='same',
-                   kernel_initializer='glorot_uniform')(inputs)
-    model = PReLU(alpha_initializer='zero',
-                  shared_axes=shax)(model)
-    # Feature smashing
-    model = myconv(filters=number_of_base_filters,
-                   kernel_size=smashConv,
-                   strides=firstStrides,
-                   padding='same',
-                   kernel_initializer='glorot_uniform')(model)
-    model = PReLU(alpha_initializer='zero',
-                  shared_axes=shax)(model)
-    # Back projection
-    up_projection_blocks = []
-    down_projection_blocks = []
-    model = up_block_2d(model, number_of_filters=number_of_base_filters,
-      kernel_size=convolution_kernel_size, strides=strides)
-    up_projection_blocks.append(model)
-    for i in range(number_of_back_projection_stages):
-        if i == 0:
-            model = down_block_2d(model, number_of_filters=number_of_base_filters,
-              kernel_size=convolution_kernel_size, strides=strides)
-            down_projection_blocks.append(model)
-            model = up_block_2d(model, number_of_filters=number_of_base_filters,
-              kernel_size=convolution_kernel_size, strides=strides)
-            up_projection_blocks.append(model)
-            model = Concatenate()(up_projection_blocks)
-        else:
-            model = down_block_2d(model, number_of_filters=number_of_base_filters,
-              kernel_size=convolution_kernel_size, strides=strides,
-              include_dense_convolution_layer=True)
-            down_projection_blocks.append(model)
-            model = Concatenate()(down_projection_blocks)
-            model = up_block_2d(model, number_of_filters=number_of_base_filters,
-              kernel_size=convolution_kernel_size, strides=strides,
-              include_dense_convolution_layer=True)
-            up_projection_blocks.append(model)
-            model = Concatenate()(up_projection_blocks)
-    outputs = myconv(filters=number_of_outputs,
-                     kernel_size=last_convolution,
-                     strides=firstStrides,
-                     padding = 'same',
-                     kernel_initializer = "glorot_uniform")(model)
-    if number_of_loss_functions == 1:
-        deep_back_projection_network_model = Model(inputs=inputs, outputs=outputs)
-    else:
-        outputList=[]
-        for k in range(number_of_loss_functions):
-            outputList.append(outputs)
-        deep_back_projection_network_model = Model(inputs=inputs, outputs=outputList)
-    return deep_back_projection_network_model
-
-
-
 
 # set up strides and patch sizes - **these could also be explored empirically**
 pszlo = 32
@@ -403,9 +207,10 @@ def my_loss_msq(y_true, y_pred  ):
 nfilt=64
 nff = 256
 convn = 6
-lastconv = 1
+lastconv = 3
+nbp=5
 ofn='./models/dsr3d_'+str(strider)+'up_' + str(nfilt) + '_' + str( nff ) + '_' + str(convn)+ '_' + str(lastconv)+ '_' + str(os.environ['CUDA_VISIBLE_DEVICES'])+'_v0.0.h5'
-mdl = dbpn( (None,None,None,1),
+mdl = src.dbpn_arch.dbpn( (None,None,None,1),
   number_of_outputs=1,
   number_of_base_filters=nfilt,
   number_of_feature_filters=nff,
